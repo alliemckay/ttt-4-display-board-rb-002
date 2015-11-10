2b3c83430f981059c76c07badb60742f963b3798
alliemckay <msarmckay@gmail.com>
2015-11-10 14:54:49 +0000

    nitrous-backup at Tue Nov 10 14:54:49 UTC 2015

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+def display_board(board)
+  
+  display = [
+      ["   ", "|", "   ", "|", "   "],
+      ["-----------"],
+      ["   ", "|", "   ", "|", "   "],
+      ["-----------"],
+      ["   ", "|", "   ", "|", "   "]
+      ]
+  
+end
